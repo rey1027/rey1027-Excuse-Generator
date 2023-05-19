e22a1fba9944e19acc6981f12edf04a5a809e026
rey1027 <62966013+rey1027@users.noreply.github.com>
2023-05-19 03:37:07 +0000

    Generador de Excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generarExcusa() {
+  //Palabras dadas
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Generar aleatoriamente un palabra
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  // Concatenando los elementos seleccionados aleatoriamente
+  let excuse =
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex];
+
+  return excuse;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuseElement = document.getElementById("excuse");
+  let excuse = generarExcusa();
+  excuseElement.innerHTML = excuse;
 };
